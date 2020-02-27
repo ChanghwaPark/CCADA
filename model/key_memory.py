ab484418b59e35e2e8949900cb2683d9d30f63e9
ChanghwaPark <omega6464@gmail.com>
2020-02-28 02:22:00 +0900

    local_dim to only batch implemented

--- model/key_memory.py
+++ model/key_memory.py
@@ -5,17 +5,16 @@ from torch import nn
 
 
 class KeyMemory(nn.Module):
-    def __init__(self, queue_size, feature_dim, local_dims=7):
+    def __init__(self, queue_size, feature_dim):
         super(KeyMemory, self).__init__()
         self.queue_size = queue_size
         self.feature_dim = feature_dim
-        self.local_dims = local_dims
+        # self.local_dims = local_dims
         # self.index = 0
 
         stdv = 1. / math.sqrt(self.feature_dim / 3)
-        self.register_buffer('features', torch.rand(self.queue_size, self.feature_dim, self.local_dims, self.local_dims)
-                             .mul_(2 * stdv).add_(-stdv))
-        print(f'Using queue shape: ({self.queue_size}, {self.feature_dim}, {self.local_dims}, {self.local_dims})')
+        self.register_buffer('features', torch.rand(self.queue_size, self.feature_dim).mul_(2 * stdv).add_(-stdv))
+        print(f'Using queue shape: ({self.queue_size}, {self.feature_dim})')
 
     def store_keys(self, batch_features, batch_indices):
         # batch_size = batch_features.size(0)
